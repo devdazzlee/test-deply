96339924c6b4c3999f5a43f0f9e85dc3a32133a0
enix403 <fog.code000@gmail.com>
2024-07-30 17:13:52 +0500

    Replace <a> with <Link>

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -18,14 +18,14 @@ const Footer: React.FC = ({}) => {
               </h2>
               <ul className='text-gray-500 font-medium'>
                 <li className='mb-4'>
-                  <a href='#' className='hover:underline'>
+                  <Link href='#' className='hover:underline'>
                     Shutter Guide
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='' className='hover:underline'>
+                  <Link href='' className='hover:underline'>
                     Listings
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -35,14 +35,14 @@ const Footer: React.FC = ({}) => {
               </h2>
               <ul className='text-gray-500 font-medium'>
                 <li className='mb-4'>
-                  <a href='#' className='hover:underline '>
+                  <Link href='#' className='hover:underline '>
                     Github
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#' className='hover:underline'>
+                  <Link href='#' className='hover:underline'>
                     Discord
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -52,14 +52,14 @@ const Footer: React.FC = ({}) => {
               </h2>
               <ul className='text-gray-500 font-medium'>
                 <li className='mb-4'>
-                  <a href='#' className='hover:underline'>
+                  <Link href='#' className='hover:underline'>
                     Privacy Policy
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href='#' className='hover:underline'>
+                  <Link href='#' className='hover:underline'>
                     Terms &amp; Conditions
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -104,7 +104,7 @@ const Footer: React.FC = ({}) => {
             </div>
           </div>
           <div className='flex mt-4 sm:justify-center sm:mt-0'>
-            <a href='#' className='text-gray-500 hover:text-gray-900 '>
+            <Link href='#' className='text-gray-500 hover:text-gray-900 '>
               <svg
                 className='w-4 h-4'
                 aria-hidden='true'
@@ -119,8 +119,8 @@ const Footer: React.FC = ({}) => {
                 />
               </svg>
               <span className='sr-only'>Facebook page</span>
-            </a>
-            <a href='#' className='text-gray-500 hover:text-gray-900 ms-5'>
+            </Link>
+            <Link href='#' className='text-gray-500 hover:text-gray-900 ms-5'>
               <svg
                 className='w-4 h-4'
                 aria-hidden='true'
@@ -131,8 +131,8 @@ const Footer: React.FC = ({}) => {
                 <path d='M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z' />
               </svg>
               <span className='sr-only'>Discord community</span>
-            </a>
-            <a href='#' className='text-gray-500 hover:text-gray-900  ms-5'>
+            </Link>
+            <Link href='#' className='text-gray-500 hover:text-gray-900  ms-5'>
               <svg
                 className='w-4 h-4'
                 aria-hidden='true'
@@ -147,8 +147,8 @@ const Footer: React.FC = ({}) => {
                 />
               </svg>
               <span className='sr-only'>Twitter page</span>
-            </a>
-            <a href='#' className='text-gray-500 hover:text-gray-900  ms-5'>
+            </Link>
+            <Link href='#' className='text-gray-500 hover:text-gray-900  ms-5'>
               <svg
                 className='w-4 h-4'
                 aria-hidden='true'
@@ -163,8 +163,8 @@ const Footer: React.FC = ({}) => {
                 />
               </svg>
               <span className='sr-only'>GitHub account</span>
-            </a>
-            <a href='#' className='text-gray-500 hover:text-gray-900 ms-5'>
+            </Link>
+            <Link href='#' className='text-gray-500 hover:text-gray-900 ms-5'>
               <svg
                 className='w-4 h-4'
                 aria-hidden='true'
@@ -179,7 +179,7 @@ const Footer: React.FC = ({}) => {
                 />
               </svg>
               <span className='sr-only'>Dribbble account</span>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
