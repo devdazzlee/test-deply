1b5bf9e04bc7197c5800835d82e48833045a9dc3
haroon sadiq <muhammadharoon9803@gmail.com>
2024-08-05 19:41:20 +0500

    footer content changed and stars colored

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -39,7 +39,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
           onClick={onRent}
           className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
         >
-          Make a listing
+          Get Started
         </div>
         <div
           onClick={toggleOpen}
