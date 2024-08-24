701baaed5dd1703152df98875cc5a24067c5d386
enix403 <fog.code000@gmail.com>
2024-08-24 17:20:34 +0500

    add approve listing button

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -7,6 +7,8 @@ import ListingCategory from "./ListingCategory";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 import RatingStars from "../RatingStars";
+import { Button } from "@nextui-org/react";
+import { IconHomeCheck } from "@tabler/icons-react";
 
 const Map = dynamic(() => import("../Map"), {
   ssr: false
@@ -47,18 +49,31 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
     setShowAllCategories(prevState => !prevState);
   };
 
-
   return (
     <div className='col-span-4 flex flex-col gap-8'>
       <div className='flex flex-col gap-2'>
+        <Button
+          color='danger'
+          variant='solid'
+          className='!font-bold mt-6 self-start mb-4'
+          size="lg"
+          endContent={<IconHomeCheck />}
+        >
+          Approve Listing
+        </Button>
+
         <div className='text-xl font-semibold flex flex-row items-center gap-2'>
           <div>Hosted by {user?.name}</div>
           <Avatar src={user?.image} />
         </div>
 
         <div className='flex items-center'>
-          {<RatingStars rating={averageRating} numberOfRatings={numberOfRatings} />}
-
+          {
+            <RatingStars
+              rating={averageRating}
+              numberOfRatings={numberOfRatings}
+            />
+          }
         </div>
 
         <div className='flex flex-row items-center gap-4 font-light text-neutral-500'>
@@ -79,8 +94,8 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
         {showAllCategories
           ? category?.map(item => <ListingCategory key={item} label={item} />)
           : category
-            ?.slice(0, 6)
-            .map(item => <ListingCategory key={item} label={item} />)}
+              ?.slice(0, 6)
+              .map(item => <ListingCategory key={item} label={item} />)}
         {/* Show More button */}
         {category?.length && category.length > 6 ? (
           <button
