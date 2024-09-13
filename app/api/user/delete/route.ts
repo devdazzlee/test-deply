27b6b4420906e0663cab7173f45e5b8bb75dccab
Shoaib Imran <Shoaib.Imran33@thehutgroup.com>
2024-09-13 21:27:36 +0100

    transactional deletion

--- app/api/user/delete/route.ts
+++ app/api/user/delete/route.ts
@@ -1,54 +1,57 @@
 import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
 import prisma from '@/app/libs/prismadb';
 import getCurrentUser from '@/app/actions/getCurrentUser';
 
 export async function DELETE() {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const userId = currentUser.id;
+
   try {
-    const currentUser = await getCurrentUser();
+    await prisma.$transaction(async (prisma: PrismaClient) => {
+      await prisma.account.deleteMany({
+        where: {
+          userId: userId,
+        },
+      });
 
-    if (!currentUser) {
-      return NextResponse.error();
-    }
+      await prisma.listing.deleteMany({
+        where: {
+          userId: userId,
+        },
+      });
 
-    const userId = currentUser.id;
+      await prisma.reservation.deleteMany({
+        where: {
+          OR: [
+            { userId: userId },
+            { receiverId: userId }
+          ],
+        },
+      });
 
-    await prisma.account.deleteMany({
-      where: {
-        userId: userId,
-      },
-    });
+      await prisma.comment.deleteMany({
+        where: {
+          userId: userId,
+        },
+      });
 
-    await prisma.listing.deleteMany({
-      where: {
-        userId: userId,
-      },
-    });
+      await prisma.subscription.deleteMany({
+        where: {
+          userId: userId,
+        },
+      });
 
-    await prisma.reservation.deleteMany({
-      where: {
-        OR: [
-          { userId: userId },       
-          { receiverId: userId }    
-        ],
-      },
-    });
-
-    await prisma.comment.deleteMany({
-      where: {
-        userId: userId,
-      },
-    });
-
-    await prisma.subscription.deleteMany({
-      where: {
-        userId: userId,
-      },
-    });
-
-    await prisma.user.delete({
-      where: {
-        id: userId,
-      },
+      await prisma.user.delete({
+        where: {
+          id: userId,
+        },
+      });
     });
 
     return NextResponse.json({ message: 'User account and all related data deleted successfully' });
