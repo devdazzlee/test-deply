84fc2180bc4334bd2f5e245fe60da2d61a142010
haroon sadiq <muhammadharoon9803@gmail.com>
2024-10-17 20:10:57 +0500

    emails sending successfully-terms and faq done

--- app/terms-and-conditions/page.tsx
+++ app/terms-and-conditions/page.tsx
@@ -1,41 +1,122 @@
-"use client";
+import Link from 'next/link';
+import React from 'react';
 
-
-export default function TermsAndConditionsPage() {
+export default function Terms() {
     return (
-        <>
-            <div className="bg-white text-black sm:pt-0 pt-8 p-4">
-                <div className="container mx-auto px-4 pt-8">
+        <div className="p-4 bg-white">
+            <div className="max-w-4xl mx-auto rounded-lg p-2 md:p-6">
+                <h1 className="text-2xl md:text-3xl font-bold text-center mt-12 sm:mt-6 mb-6">Terms and Conditions for ShutterGuide LLC</h1>
+                <p className="text-sm text-theme1 text-center mb-8">Last Updated: October 7, 2024</p>
 
-                    <div className='w-full flex'>
-                        <h1 className="text-3xl font-bold mb-4 text-theme">Terms of Service for Your Business </h1>
-                    </div>
-                    <br />
-                    <p className="mb-4 text-base">
-                        Welcome to the website. Please read these terms of service carefully before using the website. By using the website, you agree to be bound by these terms of service. If you do not agree to these terms of service, you may not use the website. These terms of service govern your use of the website and all services provided by the website. If you do not agree to these terms of service, you may not use the website.
+                <section className="mb-6">
+                    <h2 className="text-xl md:text-2xl font-semibold mb-2">Welcome to ShutterGuide LLC</h2>
+                    <p className="text-theme1">
+                        Welcome to ShutterGuide LLC. These Terms and Conditions ("Terms") govern your access to and use of our website located at <span className="text-blue-600"><Link href={'/'}>
+                            shutterguide.io
+                        </Link>
+                        </span> (the &quot;Site&quot;) and any services provided by ShutterGuide LLC (&quot;we&quot;, &quot;us&quot;, or &quot;our&quot;). By accessing or using our Site, you agree to be bound by these Terms. If you do not agree to these Terms, please do not use our Site.
                     </p>
-                    <br />
-                    <div className=''>
-                        <h2 className='font-bold text-theme'>General</h2>
+                </section>
 
-                        <br />
-                        <ul className="px-8">
-                            <li className='list-disc'>
-                                <p className="text-base">
-                                    By accessing this website, you agree to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.
-                                </p>
-                            </li>
-                            <br />
-                            <li className='list-disc'>
-                                <p className="text-base">
-                                    We reserve the right to change these terms of service at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service. Any updates you will be notified via email.
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <section className="mb-6">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">1. Acceptance of Terms</h3>
+                    <p className="text-theme1">
+                        By creating a profile and using the services provided on ShutterGuide LLC, you agree to comply with these Terms. We reserve the right to modify these Terms at any time. Your continued use of the Site after changes are made constitutes your acceptance of the new Terms.
+                    </p>
+                </section>
+
+                <section className="mb-6">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">2. Eligibility</h3>
+                    <p className="text-theme1">
+                        To use our services, you must be at least 18 years old or have the consent of a parent or guardian. By using the Site, you represent and warrant that you meet this eligibility requirement.
+                    </p>
+                </section>
+
+                <section className="mb-6">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">3. Creator Accounts</h3>
+                    <h4 className="text-lg font-medium mb-1">3.1. Profile Creation</h4>
+                    <p className="text-theme1">
+                        To become a &quot;Creator&quot; on our Site, you must create an account. You are responsible for maintaining the confidentiality of your account information and for all activities that occur under your account.
+                    </p>
+                    <h4 className="text-lg font-medium mb-1">3.2. Data and Content</h4>
+                    <p className="text-theme1">
+                        You are responsible for the data and content you upload to your profile, including photography and videography work. You must ensure that all content is your own or that you have the necessary rights and permissions to use and share it.
+                    </p>
+                    <h4 className="text-lg font-medium mb-1">3.3. Compliance with Laws</h4>
+                    <p className="text-theme1">
+                        You agree to comply with all applicable laws and regulations when using our Site and your Creator account.
+                    </p>
+                </section>
+
+                <section className="mb-6">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">4. Payment Processing</h3>
+                    <h4 className="text-lg font-medium mb-1">4.1. Stripe Onboarding</h4>
+                    <p className="text-theme1">
+                        Weuse Stripe, a third-party payment processor, to manage payments to Creators. By using our Site, you agree to comply with Stripe&apos;s Terms of Service and Privacy Policy. You authorize us to charge your designated account for any applicable fees related to your use of our services.
+                    </p>
+                    <h4 className="text-lg font-medium mb-1">4.2. Fees</h4>
+                    <p className="text-theme1">
+                        ShutterGuide LLC reserves the right to charge Creators fees for using our services. Any fees will be disclosed prior to the transaction. You are responsible for any taxes applicable to your payments.
+                    </p>
+                </section>
+
+                <section className="mb-6">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">5. License Grant</h3>
+                    <p className="text-theme1">
+                        By submitting content to your profile, you grant ShutterGuide LLC a non-exclusive, royalty-free, perpetual, and worldwide license to use, display, reproduce, and distribute the content on our Site and for promotional purposes.
+                    </p>
+                </section>
+                <section className="mb-6">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">6. Intellectual Property</h3>
+                    <p className="text-theme1">
+                        All content and materials on the Site, including text, graphics, logos, and software, are the property of ShutterGuide LLC or its licensors and are protected by copyright and other intellectual property laws. You may not reproduce, modify, or distribute any of our materials without our prior written consent.
+                    </p>
+                </section>
+                <section className="mb-6">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">7. Termination</h3>
+                    <p className="text-theme1">
+                        We reserve the right to suspend or terminate your account and access to the Site at our sole discretion, without notice, if you violate these Terms or engage in any activity that we deem harmful to the Site or our users.
+                    </p>
+                </section>
+
+
+                <section className="mb-6">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">8. Disclaimers</h3>
+                    <p className="text-theme1">
+                        The Site and our services are provided on an &quot;as-is&quot; basis without warranties of any kind, either express or implied. We do not warrant that the Site will be uninterrupted, secure, or free from errors.
+                    </p>
+                </section>
+                <section className="mb-6">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">9. Limitation of Liability</h3>
+                    <p className="text-theme1">
+                        To the fullest extent permitted by law, ShutterGuide LLC shall not be liable for any indirect, incidental, special, consequential, or punitive damages arising from or related to your use of the Site or services, even if we have been advised of the possibility of such damages.
+                    </p>
+                </section>
+                <section className="mb-6">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">10. Indemnification</h3>
+                    <p className="text-theme1">
+                        You agree to indemnify and hold harmless ShutterGuide LLC, its affiliates, and their respective officers, directors, employees, and agents from any claims, liabilities, damages, losses, or expenses, including reasonable attorney fees, arising out of or in connection with your use of the Site, your violation of these Terms, or your infringement of any third-party rights.
+                    </p>
+                </section>
+                <section className="mb-6">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">11. Governing Law</h3>
+                    <p className="text-theme1">
+                        These Terms shall be governed by and construed in accordance with the laws of the State of [insert state], without regard to its conflict of law principles.
+                    </p>
+                </section>
+                <section className="mb-6">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">12. Contact Information</h3>
+                    <p className="text-theme1 mb-8">
+                        If you have any questions about these Terms, please contact us at:
+                    </p>
+                    <p className="font-bold">**ShutterGuide LLC**</p>
+                    <a href="mailto:shutterguidellc@gmail.com" className="text-blue-600">shutterguidellc@gmail.com</a>
+                </section>
+
+                <p className="text-theme1 text-sm">
+                    By using ShutterGuide LLC, you acknowledge that you have read, understood, and agree to these Terms and Conditions.
+                </p>
             </div>
-        </>
+        </div>
     );
 }
-
